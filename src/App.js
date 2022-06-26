0c70ef23d390a555fde68d4eaaeef48f470328e1
BoccoSantiago <italobocco@gmail.com>
2022-06-26 17:31:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+//import Hero from "./components/Hero"
+import Card from "./components/Card"
 import "./style.css"
+import katie from './images/katie-zaferes.png' 
 /*
 Challenge: Build the Navbar component.
 Check the Figma file for the design specifics.
 */
 
 export default function App() {
+    // <Hero />
     return (
         <div>
             <Navbar />
+            <Card
+            img={katie}
+            rating="5.0"
+            reviewCount = {6}
+            country= "USA"
+            title="Life Lessons with Katie Zaferes"
+            price={136}
+            />
         </div>
     )
 }
